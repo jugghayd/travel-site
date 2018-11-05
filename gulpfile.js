8584e11abd9fef51fdf5f8634481cf1a775e421a
Christian Higgins <christian.higgins@gmail.com>
2018-11-05 12:39:00 -0500

    Completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+/*
+gulp.src() -- Where to pull water from ('./main.css')
+gulp.dest() -- where to place the water once it's done running through the pipes ('.temp/styles')
+*/
+
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
