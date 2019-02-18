7d59bb47e28e54eaab44416eff5f35067074bdad
Christian Higgins <christian.higgins@gmail.com>
2019-02-18 11:17:57 -0500

    Completed desktop and mobile header styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-/*
-gulp.src() -- Where to pull water from ('./main.css')
-gulp.dest() -- where to place the water once it's done running through the pipes ('.temp/styles')
-*/
-
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
+
+
+
+
+
